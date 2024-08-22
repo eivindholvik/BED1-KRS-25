87f6c30f6325f71ac1f12bde321c150ab9e75b0d
eivindholvik <eivind.holvik@noroff.no>
2024-08-22 13:08:00 +0200

    Add extra tasks for module 1 week 1

---
+++ 01-programming_foundations/module_1/day_5/extra_tasks.js
@@ -0,0 +1,44 @@
+// Task1
+// Create an array of your favorite fruits and print the first and last elements.
+
+// Task 2
+// Create an array of numbers and calculate the sum of all elements.
+
+// Task 3
+// Create an array of strings and find the longest string.
+
+// Task 4
+// Create an array of numbers and find the median value.
+
+// Task 5
+// Create an array of strings and remove duplicate elements.
+// Hint, research Set
+
+// Task 6
+// Reverse this string: "Hello". The string should read "Olleh" (Correct casing)
+
+// Task 7
+// Count the number of vowels in a string. Use the string "Hello world"
+
+// Task 8
+// Check if a string is a palindrome
+// The function should return true if the string is a palindrome.
+// A palindrome is a string that is the same backwards.
+
+// Task 9 - hard (easier with the use of objects)
+// Implement a function to find the most frequent character in a string.
+
+// Task 10
+// Create a function to convert Celsius to Fahrenheit.
+// Formula: (32°F − 32) × 5/9 = 0°C
+
+// Task 11
+// Create a function to calculate the area of a triangle.
+// Formula: (base * height) / 2
+
+// Task 12
+// Print numbers from 1 to 10 using a for loop.
+
+// Task 13 - hard
+// Print the Fibonacci sequence up to 10 terms using a do-while loop.
+// The Fibonacci sequence ( 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...) starts with 0, 1. It sums the two previous numbers to find the new number. F0 = 0, F1 = 1, Fn = Fn-1 + Fn-2
