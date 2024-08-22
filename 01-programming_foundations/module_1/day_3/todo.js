e6612c25c4b15d3dd1c725a91cbafffe9aa19a4c
eivindholvik <eivind.holvik@noroff.no>
2024-08-22 09:14:07 +0200

    add solution to todo task

--- 01-programming_foundations/module_1/day_3/todo.js
+++ 01-programming_foundations/module_1/day_3/todo.js
@@ -13,11 +13,25 @@ form.addEventListener("submit", (e) => {
 
 function draw() {
   ul.innerHTML = "";
-  todos.forEach((ele) => {
-    ul.innerHTML += `<li>${ele}</li>`;
+  todos.forEach((ele, i) => {
+    const li = document.createElement("li");
+    li.textContent = ele;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "remove";
+    deleteBtn.style.marginLeft = "10px";
+    deleteBtn.addEventListener("click", () => {
+      deleteTodo(i);
+    });
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
   })
 }
 
+
+function deleteTodo(i) {
+  todos.splice(i, 1);
+  draw();
+}
 
 
 
