27cf160849e78e106439b2e2dd6d0aa3bc48adb9
eivindholvik <eivind.holvik@noroff.no>
2024-10-14 10:44:01 +0200

    startcode

---
+++ 03-programming_with_objects/module_1/day_1/database.js
@@ -0,0 +1,69 @@
+const students = [
+  {
+    id: 0,
+    f_name: "Eivind",
+    l_name: "Holvik",
+    email: "eivind.holvik@noroff.no",
+    tlf: "99514780",
+    courses: [
+      0, 2
+    ]
+  },
+  {
+    id: 1,
+    f_name: "Eline",
+    l_name: "Holvik",
+    email: "eline.holvik@noroff.no",
+    tlf: "95144859"
+  },
+  {
+    id: 3,
+    f_name: "Frank",
+    l_name: "Jensen",
+    email: "frank.jensen@noroff.no",
+    tlf: "99554780"
+  }
+];
+
+const courses = [
+  {
+    id: 0,
+    abr: "Gym",
+    name: "Gymnastikk",
+    points: 15
+  }, {
+    id: 1,
+    abr: "Matte1",
+    name: "Matematikk 1",
+    points: 15
+  }, {
+    id: 2,
+    abr: "FET",
+    name: "Front End Technologies",
+    points: 30
+  }
+];
+
+const teachers = [
+  {
+    id: 0,
+    f_name: "Jens",
+    l_name: "Trulsen",
+    teaching: [
+      2
+    ]
+  },
+  {
+    id: 1,
+    f_name: "Johanne",
+    l_name: "Holm",
+    teaching: [
+      2
+    ]
+  },
+  {
+    id: 3,
+    f_name: "Marvin",
+    l_name: "Mikalsen",
+  }
+];
