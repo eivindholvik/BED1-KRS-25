3ce5e9c2f7386422e1e45c2a7c7f2a3b086d9671
eivindholvik <eivind.holvik@noroff.no>
2025-08-22 09:59:37 +0200

    add frequent_char kata

---
+++ KATA/refresh-brain-26/find_frequent_char.js
@@ -0,0 +1,4 @@
+// Write a function that takes a string
+// It should count all characters
+// It should output the most frequent, with it's frequency
+// Add another optional parameter that allows the output to include n numbers of characters with its corresponding frequency, in descending order
